e1317bcfcbbc56516a92091ba1d0b15a18163b9e
dannylui91 <dannylui91@gmail.com>
2016-09-03 16:17:26 -0400

    added DannyLui.java

---
+++ src/nyc/c4q/DannyLui.java
@@ -0,0 +1,23 @@
+package nyc.c4q;
+
+public class DannyLui implements C4QStudent {
+    @Override
+    public String name() {
+        return "Danny Lui";
+    }
+
+    @Override
+    public String nickName() {
+        return "Dan";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 10;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "Orange";
+    }
+}
