41a809fef3389a3ca8279ed64dc69d0253758fdc
Max Rosado <max.a.rosado@gmail.com>
2016-09-03 16:29:23 -0400

    Sept 3rd working with git

---
+++ src/nyc/c4q/MaxRosado.java
@@ -0,0 +1,26 @@
+package nyc.c4q;
+
+/**
+ * Created by maxrosado on 9/3/16.
+ */
+public class MaxRosado implements C4QStudent {
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public String nickName() {
+        return null;
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 0;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return null;
+    }
+}
