ff707296daf004f9d3f75466d2982dbbc2a90512
Jonathan Colon <jonatron2.0@gmail.com>
2016-09-03 16:32:49 -0400

    Added Jonathan.java class

---
+++ src/nyc/c4q/Jonathan.java
@@ -0,0 +1,26 @@
+package nyc.c4q;
+
+/**
+ * Created by jonathancolon on 9/3/16.
+ */
+public class Jonathan implements C4QStudent{
+    @Override
+    public String name() {
+        return "Jonathan Colon";
+    }
+
+    @Override
+    public String nickName() {
+        return "J0NATR0N";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 17000;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "Olive Green";
+    }
+}
