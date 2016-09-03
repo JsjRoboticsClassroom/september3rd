4f9ff370adf9ca65a51fde2c9c44e6df830150ef
Asia Gibson <asiagibson@C4Qs-MacBook-Pro-20.local>
2016-09-03 16:30:18 -0400

    added implement C4Q  Asia

---
+++ src/nyc/c4q/Asiagibson.java
@@ -0,0 +1,26 @@
+package nyc.c4q;
+
+/**
+ * Created by asiagibson on 9/3/16.
+ */
+public class Asiagibson implements C4QStudent{
+    @Override
+    public String name() {
+        return "Asia";
+    }
+
+    @Override
+    public String nickName() {
+        return "AJ";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 3000;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "Orange";
+    }
+}
