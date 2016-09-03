43a56d17bc1b96d703f470d85d5df5813a4f5639
Rafael Soto <rooksoto@gmail.com>
2016-09-03 16:30:13 -0400

    Add Implement C4QStudent

---
+++ src/nyc/c4q/Rafael.java
@@ -0,0 +1,25 @@
+package nyc.c4q;
+
+public class Rafael implements C4QStudent {
+
+
+    @Override
+    public String name() {
+        return "Rafael";
+    }
+
+    @Override
+    public String nickName() {
+        return "Rook";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 2000;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "Purple";
+    }
+}
