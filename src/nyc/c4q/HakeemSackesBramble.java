beb941327adacd29496c0cb52eef42f8269e9a15
HakeemSackes-Bramble <hakksaxx@outlook.com>
2016-09-03 16:31:39 -0400

    added java class with name

---
+++ src/nyc/c4q/HakeemSackesBramble.java
@@ -0,0 +1,28 @@
+package nyc.c4q;
+
+/**
+ * Created by hakeemsackes-bramble on 9/3/16.
+ */
+public class HakeemSackesBramble implements C4QStudent{
+
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public String nickName() {
+        return null;
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 0;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return null;
+    }
+}
