a364eb419e1d4e9d1a9ec164517a3ee33b79c09e
CurrentlyJordan <jwsmith451@gmail.com>
2016-09-03 16:30:21 -0400

    added an implementation of c4q student Jordan Smith

---
+++ src/nyc/c4q/Jordan.java
@@ -0,0 +1,20 @@
+package nyc.c4q;
+
+/**
+ * Created by jordansmith on 9/3/16.
+ */
+public class Jordan implements C4QStudent {
+    public String name(){
+        return "Jordan";
+    }
+    public String nickName(){
+        return "JJ";
+    }
+    public int stepsPerDay(){
+        return 500;
+    }
+    public String yourFavoriteColor(){
+        return "blue";
+    }
+
+}
