a234903fc3192653adfad22e79b7fe2f4787054e
Cat Wong <catwong.nyc@gmail.com>
2016-09-03 16:30:20 -0400

    added CatherineWong.java file

---
+++ src/nyc/c4q/CatherineWong.java
@@ -0,0 +1,26 @@
+package nyc.c4q;
+
+/**
+ * Created by CATWONG on 9/3/16.
+ */
+public class CatherineWong implements C4QStudent{
+    @Override
+    public String name() {
+        return "Catherine Wong";
+    }
+
+    @Override
+    public String nickName() {
+        return "Cat";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 3638;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "Black";
+    }
+}
