1a1f1d98db30a6178ca426484a096e219c9b199d
Ridita Nizam <riditan@gmail.com>
2016-09-03 16:32:56 -0400

    I wish I could watch tv

---
+++ src/nyc/c4q/RiditaNizam.java
@@ -0,0 +1,23 @@
+package nyc.c4q;
+
+/**
+ * Created by Ridita on 9/3/16.
+ */
+public class RiditaNizam implements C4QStudent {
+    @Override
+    public String name() {
+        return "Ridita";
+    }
+    @Override
+    public String nickName() {
+        return "Ridi";
+    }
+    @Override
+    public int stepsPerDay() {
+        return 0;
+    }
+    @Override
+    public String yourFavoriteColor() {
+        return "coral";
+    }
+}
