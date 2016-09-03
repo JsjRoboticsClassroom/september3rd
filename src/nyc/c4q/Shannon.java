3f7df75a114bc9cc36e1b545806740d090cc24ac
Shannon Alexander-Navarro <shannonalexander-navarro@C4Qs-MacBook-Pro-3.local>
2016-09-03 16:30:30 -0400

    Shannon's commit

---
+++ src/nyc/c4q/Shannon.java
@@ -0,0 +1,26 @@
+package nyc.c4q;
+
+/**
+ * Created by shannonalexander-navarro on 9/3/16.
+ */
+public class Shannon implements C4QStudent{
+    @Override
+    public String name() {
+        return "Shannon";
+    }
+
+    @Override
+    public String nickName() {
+        return "Master of the Universe";
+    }
+
+    @Override
+    public int stepsPerDay() {
+        return 1000000;
+    }
+
+    @Override
+    public String yourFavoriteColor() {
+        return "gooooold";
+    }
+}
